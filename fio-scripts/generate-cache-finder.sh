df9c9c170c91311be4b12a345743afd79f5a2d01
Gary <gary.j.little@gmail.com>
2022-06-27 07:45:42 -0400

    clean up usage

--- fio-scripts/generate-cache-finder.sh
+++ fio-scripts/generate-cache-finder.sh
@@ -12,7 +12,7 @@ WSS_SMALL=(8m 16m 32m 64m 128m 256m 512m 1g)
 WSS=${WSS_FULL[*]}
 #Define block sizes - can be overridden with -b switch.
 BS=4k
-#Define queue depth
+#Define queue depth - can be overridden with -q switch 
 IODEPTH=64
 #Define runtime per iteration
 RUNTIME=15s
@@ -31,14 +31,8 @@ DIRECT=1
 
 
 #############################################################
-#
-# Take some parameters from the command line.  Namely the
-# destination directory where the fio files will be written
-# to (-d) and the file/device under test (-f).  It is 
-# probably best to have the user specify the file on the 
-# command line to avoid accidentally overwriting a mounted
-# filesystem / device.
-#
+# Main function that does the work of creating fio files in
+# the destination directory
 #############################################################
 write_fio_file() {
 	let count=0
@@ -74,22 +68,31 @@ usage() {
 	printf "Usage: $0 -d <output directory> -f <file or device name> -b <blocksize> -h Get Help\n\n"
 }
 help() { 
-	printf "This script generates a set of fio files that can be used to generate IO across a \
+	printf "\n\nThis script generates a set of fio files that can be used to generate IO across a \
 range of working set sizes wss.\n\n \
 -f "filename" which can be a file-system file or device file
 \n \
--d "directory" the output directory where the fio files will be written. \n\n\
-After creating the fio files, the script run-cache-finder.sh can be run to execute the fio files in order\n\n
-\n\
--b "blocksize" Blocksize passed to fio e.g. 8k or 1m \n\n"	
+-d "directory" the output directory where the fio files will be written.
+\n \
+-b "blocksize" Blocksize passed to fio e.g. 8k or 1m  default 4k
+\n \
+-q "queuedepth" (iodepth) passed to fio default 64 
+\n \
+After creating the fio files, the script run-cache-finder.sh can be run to execute the fio files in order\n\n"
 	exit 1
 }
 #Check for empty argument list
 [[ $# -eq 0 ]] && { usage ; exit 1; }
 
-
 #############################################################
-# parse and validate command line options
+#
+# Take some parameters from the command line.  Namely the
+# destination directory where the fio files will be written
+# to (-d) and the file/device under test (-f).  It is 
+# probably best to have the user specify the file on the 
+# command line to avoid accidentally overwriting a mounted
+# filesystem / device.
+#
 #############################################################
 while getopts ":f:d:b:h" Option
 do
@@ -97,6 +100,7 @@ do
         d   )   OUTPUTDIR=$OPTARG ;;
         f   )   DEVICE=$OPTARG ;;
         b   )   BS=$OPTARG ;;
+        q   )   IODEPTH=$OPTARG ;;
         h   )   help ;;
 	*   )   echo usage ;;
     esac
