abd5bc121ba8ad30233066e4fc9a4740aba95c34
Gary <gary.j.little@gmail.com>
2022-07-06 17:00:15 -0400

    Helper script

---
+++ fio-scripts/runsweep.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S bash 
+
+###############################################
+# Run the complete sweep process
+#
+# 1. Generate the fio files
+# 2. Execute the fio scripts
+# 4. Process the results
+# 5. Plot the results
+###############################################
+
+# Pass in sweep script, base directory, list of devices
+
+SWEEPSCRIPT=$1
+BASEDIR=$2
+DEVICE=$3
+
+RESDIR=$(./$SWEEPSCRIPT -d $BASEDIR -f $DEVICE|tail -1)
+sudo ./run-fio-files.sh -d $RESDIR 
+./process-fio-yaml.sh -d $RESDIR
+
+if [[ $# -gt 3 ]] ; then 
+	shift
+	for i in $@
+	do
+		shift
+		DEVICE=$1
+		if [[ ! -z $DEVICE ]] ; then 
+			RESDIR=$(./$SWEEPSCRIPT -d $BASEDIR -f $DEVICE|tail -1)
+			sudo ./run-fio-files.sh -d $RESDIR 
+			./process-fio-yaml.sh -d $RESDIR
+		fi
+	done
+fi
+
+
+
